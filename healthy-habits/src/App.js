559dafa3648fa930308d2219bd7736bd7b06c23c
Owner <braydens.coding@gmail.com>
2025-05-20 18:56:25 -0500

    Completed studio

--- healthy-habits/src/App.js
+++ healthy-habits/src/App.js
@@ -6,34 +6,54 @@ import "./App.css";
 function App() {
 
     // TODO: Create a state variable to hold the five numbers to track. This should be an object with five properties: steps, water, food, exercise, and net. They should each be initialized with with a zero value (as a number).  Notice the hook you need has been imported at the top.
-
+    const [numbers, setNumbers] = useState({
+        steps: 0,
+        water: 0,
+        food: 0,
+        exercise: 0,
+        net: 0
+    })
 
     // TODO: Create a state variable to track which tab should be displayed.
     // Initialize it to the string "net".
-    
+    const [tab, setTab] = useState("net")
 
     // TODO: Inside each of the following handlers, update the corresponding numbers property from state.
     const handleUpdateSteps = (amount) => {
         // Use prevState to change just the steps property within the numbers object.
-        
+        setNumbers((prevState) => ({
+            ...prevState,
+            steps: amount
+        }))
     };
     const handleUpdateWater = (amount) => {
         // Use prevState to change just the water property within the numbers object.
-
+        setNumbers((prevState) => ({
+            ...prevState,
+            water: amount
+        }))
     };
     const handleUpdateFood = (amount) => {
         // You'll need to update both the food calories and the net calories here (add calories consumed). Use prevState to ensure you are adding the new amount to the existing total.
-
+        setNumbers((prevState) => ({
+            ...prevState,
+            food: prevState.food + amount,
+            net: prevState.net + amount
+        }))
     };
     const handleUpdateExercise = (amount) => {
         // You'll need to update both the exercise calories and the net calories here (subtract calories burned). Use prevState to ensure you are adding the new amount to the existing total. 
-
+        setNumbers((prevState) => ({
+            ...prevState,
+            exercise: prevState.exercise + amount,
+            net: prevState.net - amount
+        }))
     };
 
     // TODO: Change the current type of details to be displayed.
     const handleCurrTypeChange = (type) => {
         // Use the setter you created with your state variable.
-        
+        setTab(type);
     };
 
     // JSX to display all content on page
@@ -45,7 +65,7 @@ function App() {
             </header>
 
             {/* TODO: Pass the numbers object into the Chart component as a prop */}
-            <Chart />
+            <Chart numbers = {numbers}/>
 
             <div className="tabs-container">
                 <div
@@ -86,7 +106,12 @@ function App() {
             
             {/* TODO: Pass the current type into the Details component */}
             {/* TODO: Pass the four handler functions to the Details component below. Check the propTypes object at the bottom of Details.js to see what the names of the attributes should be, then look just below the state variables at the top of this file for the names of the functions you are passing down. */}
-            <Details />
+            <Details 
+            type={tab}
+            updateSteps={handleUpdateSteps}
+            updateWater={handleUpdateWater}
+            updateFood={handleUpdateFood}
+            updateExercise={handleUpdateExercise}/>
             
         </div>
     );
--- healthy-habits/src/components/details.js
+++ healthy-habits/src/components/details.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import uuid from "react-uuid"; // Universally Unique ID generator
 import "../App.css";
@@ -8,20 +8,26 @@ const Details = (props) => {
     // TODO: Create state variables to hold values of form input fields.
     // Initialize them as empty strings.
     // Don't forget to import the hook you need.
-
+    const [water, setWater] = useState("");
+    const [steps, setSteps] = useState("");
+    const [foodDescription, setFoodDescription] = useState("");
+    const [foodCalories, setFoodCalories] = useState("");
+    const [exerciseDescription, setExerciseDescription] = useState("");
+    const [exerciseCalories, setExerciseCalories] = useState("");
 
     // TODO: Create state variables to hold full lists of food and exercise
     // Initialize them as empty arrays.
-
+    const [foodList, setFoodList] = useState([]);
+    const [exerciseList, setExerciseList] = useState([]);
 
     // Steps input handler (note e = event)
     const handleStepsChange = e => {
         e.preventDefault();
         let amount = Number(e.target.value);
         // TODO: set the new value in state
-        
+        setSteps(amount);
         // TODO: call the update handler via props and pass in the amount
-        
+        props.updateSteps(amount);
     }
 
     // Water input handler
@@ -29,9 +35,9 @@ const Details = (props) => {
         e.preventDefault();
         let amount = Number(e.target.value);
         // TODO: set the new value in state
-        
+        setWater(amount);
         // TODO: call the update handler via props and pass in the amount
-        
+        props.updateWater(amount);
     }
 
     // Food input handlers
@@ -39,27 +45,29 @@ const Details = (props) => {
         e.preventDefault();
         let desc = e.target.value;
         // TODO: set the new value in state
-        
+        setFoodDescription(desc)
     }
     const handleFoodCalChange = e => {
         e.preventDefault();
         let amount = Number(e.target.value);
         // TODO: set the new value in state
-        
+        setFoodCalories(amount)
     }
     const handleSubmitFood = e => {
         e.preventDefault();
         let newFoodEntry = {
             id: uuid(),
             // TODO: Add desc & cal properties using values from state
-            
+            desc: foodDescription,
+            cal: foodCalories,
         }
         // TODO: use prevState to add the new entry to the food list (once you've created it later)
-        
+        setFoodList((prevList) => [...prevList, newFoodEntry])
         // TODO: call the update handler via props and pass in the calories
-        
+        props.updateFood(foodCalories)
         // TODO: reset the food desc and cal input values to an empty string
-        
+        setFoodDescription("")
+        setFoodCalories("")
     }
 
     // Exercise input handlers
@@ -67,27 +75,29 @@ const Details = (props) => {
         e.preventDefault();
         let desc = e.target.value;
         // TODO: set the new value in state
-        
+        setExerciseDescription(desc)
     }
     const handleExerciseCalChange = e => {
         e.preventDefault();
         let amount = Number(e.target.value);
         // TODO: set the new value in state
-        
+        setExerciseCalories(amount)
     }
     const handleSubmitExercise = e => {
         e.preventDefault();
         let newExerciseEntry = {
             id: uuid(),
             // TODO: Add desc & cal properties using values from state
-            
+            desc: exerciseDescription,
+            cal: exerciseCalories
         }
         // TODO: use prevState to add the new entry to the exercise list (once you've created it later)
-        
+        setExerciseList((prevList) => [...prevList, newExerciseEntry])
         // TODO: call the update handler via props and pass in the calories from newExerciseEntry
-        
+        props.updateExercise(exerciseCalories)
         // TODO: reset the exercise desc and cal input values to an empty string
-        
+        setExerciseDescription("")
+        setExerciseCalories("")
     }
 
     // Small helper components like this don't need a separate file
@@ -130,7 +140,7 @@ const Details = (props) => {
                 <form id="water-form" onSubmit={(e) => e.preventDefault() }>
                     <p className="label">TOTAL GLASSES OF WATER</p>
                     {/* TODO: Add two-way binding for the input below */}
-                    <input type="number" placeholder="Total" min="0" />           
+                    <input type="number" placeholder="Total" min="0" value={water} onChange={handleWaterChange}/>           
                 </form>
                 <p className="list subheader">How much water should you drink each day?</p>
                 <p className="info">In general, you should drink between &frac12; - 1 ounce for every pound you weigh. That's 5-10 glasses per day for a 160-lb person.</p>
@@ -140,7 +150,7 @@ const Details = (props) => {
                 <form id="steps-form" onSubmit={(e) => e.preventDefault() }>
                     <p className="label">TOTAL STEPS</p>
                     {/* TODO: Add two-way binding for the input below */}
-                    <input type="number" placeholder="Total" min="0" />
+                    <input type="number" placeholder="Total" min="0" value={steps} onChange={handleStepsChange}/>
                 </form>
                 <p className="subheader steps-subheader">What's your daily steps goal?</p>
                 <table className="steps-table">
@@ -169,34 +179,38 @@ const Details = (props) => {
                 <form id="food-form">
                     <p className="label">ADD CALORIES CONSUMED</p>
                     {/* TODO: Add two-way binding for each of the two inputs below */}
-                    <input type="text" className="wide" placeholder="Description of food or beverage" maxLength="32" />
-                    <input type="number" placeholder="Calories" min="0" />
+                    <input type="text" className="wide" placeholder="Description of food or beverage" maxLength="32" value={foodDescription} onChange={handleFoodDescChange}/>
+                    <input type="number" placeholder="Calories" min="0" value={foodCalories} onChange={handleFoodCalChange}/>
                     {/* TODO: BONUS! Make the button disabled if either field is empty */}
-                    <button type="submit" onClick={handleSubmitFood}>Add</button>
+                    <button type="submit" onClick={handleSubmitFood} disabled={!foodDescription || !foodCalories}>Add</button>
                 </form>
                 {/* TODO: BONUS! Make the log & table appear only when the first entry is submitted */}
+                {foodList.length > 0 &&
                 <div>
                     <p className="details-header">FOOD & BEVERAGE LOG</p>
                     {/* TODO: Add the DetailsTable component and pass in the food list using the attribute 'list' */}
-                    
+                    <DetailsTable list={foodList}/>
                 </div>
+                }
             </div>}
 
             {props.type === "exercise" && <div>
                 <form id="exercise-form">
                     <p className="label">ADD CALORIES BURNED</p>
                     {/* TODO: Add two-way binding for each of the two inputs below */}
-                    <input type="text" className="wide" placeholder="Description of exercise activity" maxLength="32" />
-                    <input type="number" placeholder="Calories" min="0" />
+                    <input type="text" className="wide" placeholder="Description of exercise activity" maxLength="32" value={exerciseDescription} onChange={handleExerciseDescChange}/>
+                    <input type="number" placeholder="Calories" min="0" value={exerciseCalories} onChange={handleExerciseCalChange}/>
                     {/* TODO: BONUS! Make the button disabled if either field is empty */}
-                    <button type="submit" onClick={handleSubmitExercise}>Update</button>
+                    <button type="submit" onClick={handleSubmitExercise} disabled={!exerciseDescription || !exerciseCalories}>Update</button>
                 </form>
                 {/* TODO: BONUS! Make the log & table appear only when the first entry is submitted */}
+                {exerciseList.length > 0 &&
                 <div>
                     <p className="details-header">EXERCISE LOG</p>
                     {/* TODO: Add the DetailsTable component and pass in the exercise list using the attribute 'list' */}
-                    
+                    <DetailsTable list={exerciseList}/>
                 </div>
+                }
             </div>}
 
         </div>
